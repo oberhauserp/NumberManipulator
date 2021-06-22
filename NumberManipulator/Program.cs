5c67d235f3fba09fd7498fd16e600eb79ea7e4df
Oberhauser <34341@nelnet.net>
2021-06-22 16:10:10 -0500

    final code

--- NumberManipulator/Program.cs
+++ NumberManipulator/Program.cs
@@ -7,9 +7,8 @@ namespace NumberManipulator
         static void Main(string[] args)
         {
             string entered = Console.ReadLine();
-            int castedenter = 0;
-            Int32.TryParse(entered, out castedenter);
-            int answer = Math.Pow(castedenter, 2) + 14 - 3;
+            double castedenter = Convert.ToDouble(entered);
+            double answer = Math.Pow(castedenter, 2) + 14 - 3;
             Console.WriteLine(answer);
         }
     }
