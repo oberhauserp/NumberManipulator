6c8815dafb5087381853fdf2110edbd8fa870c14
Oberhauser <34341@nelnet.net>
2021-06-22 16:05:38 -0500

    added code

---
+++ NumberManipulator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NumberManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string entered = Console.ReadLine();
+            int castedenter = 0;
+            Int32.TryParse(entered, out castedenter);
+            int answer = Math.Pow(castedenter, 2) + 14 - 3;
+            Console.WriteLine(answer);
+        }
+    }
+}
